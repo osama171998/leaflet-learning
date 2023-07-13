15ead6dd707ab56afab8308f37e9997662571b15
Osama Idrees <osamaidrees171998@gmail.com>
2023-07-13 12:06:21 +0500

    Create separate component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import './App.css';
-import {useState} from "react"
-import {MapContainer,TileLayer,Marker,Tooltip} from "react-leaflet"
+import Map from "./components/map"
 function App() {
-  const mapTilerKey = "KM6Vos42AC6FhO7CGdw8"
-  const [maps,setMap] = useState(`https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=${mapTilerKey}`)
 
-  const availableMaps = {
-    "Basic":`https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=${mapTilerKey}`,
-    "Bright":`https://api.maptiler.com/maps/bright-v2/{z}/{x}/{y}.png?key=${mapTilerKey}`,
-    "Ocean":`https://api.maptiler.com/maps/ocean/{z}/{x}/{y}.png?key=${mapTilerKey}`
-  }
   
   return (
     <div className="App">
-        <MapContainer center={[30.3753, 69.3451]} zoom={5}scrollWheelZoom={true} >
-          <TileLayer
-          // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url={maps}
-          />
-          <Marker
-            key={1}
-            position={[30.3753, 69.3451]}
-            title="Pakistan"
-          />
-          <Tooltip direction='right' offset={[0, 0]} opacity={1} permanent>Pakistan</Tooltip>
-        </MapContainer>
-        <div className='vertical-panel'>
-              {
-                Object.keys(availableMaps).map((mapName,key)=>{
-                  return <p key={key} onClick={()=>setMap(availableMaps[mapName])}>
-                      {mapName}
-                  </p>
-                })
-              }
-        </div>
+          <Map/>
     </div>
   );
 }
